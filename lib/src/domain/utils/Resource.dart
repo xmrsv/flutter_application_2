46f1518fee4de9dc680fc81a9df4d2a6a359d0cb
xmrsv <88881293+xmrsv@users.noreply.github.com>
2024-09-03 20:50:26 -0500

    Procesador respuestas

---
+++ lib/src/domain/utils/Resource.dart
@@ -0,0 +1,15 @@
+abstract class Resource<T> {}
+
+class Initial extends Resource {}
+
+class Loading extends Resource {}
+
+class Success<T> extends Resource<T> {
+  final T data;
+  Success(this.data);
+}
+
+class Error<T> extends Resource<T> {
+  final T message;
+  Error(this.message);
+}
